4b626330b3e83d25dc6041c653c055d5c402d36a
Adrian Mutuleanu <adrian.mutuleanu@gmail.com>
2019-03-24 22:37:36 +0200

    Add carousel

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
-$('.carousel').slick({
-  slidesToShow: 4,
-  slidesToScroll: 4
-});	
+$(document).ready(function() {
+	$('.carousel').slick({
+		centerMode: true,
+		dots: true,
+		infinite: true,
+		slidesToShow: 2,
+  		slidesToScroll: 1
+	});
+});
