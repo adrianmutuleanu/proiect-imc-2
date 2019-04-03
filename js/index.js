42c1a72644a634cc1ec83cf3bb70738fb224f370
Adrian Mutuleanu <adrian.mutuleanu@gmail.com>
2019-04-03 18:44:52 +0300

    Finish without IE

--- js/index.js
+++ js/index.js
@@ -1,21 +1,16 @@
-$(document).ready(function() {
-	$('.carousel').slick({
-		dots: true,
-		infinite: true,
-		speed: 300,
-		slidesToShow: 1,
-		centerMode: true,
-		variableWidth: true,
-		responsive: [{
-			breakpoint: 1100,
-			settings: {
-				centerMode: false,
-				variableWidth: false,
-			}
-		}]
-	});
+$('.carousel').slick({
+	dots: true,
+	centerMode: true,
+	variableWidth: true,
+	responsive: [{
+		breakpoint: 1100,
+		settings: {
+			centerMode: false,
+			variableWidth: false,
+		}
+	}]
+});
 
-	$('.navbar__link-toggle').click(function () {
-		$('.navbar__items').toggleClass('navbar__toggleShow');
-	});
+$('.navbar__link-toggle').on('click', function () {
+	$('.navbar__items').toggleClass('navbar__toggleShow');
 });
